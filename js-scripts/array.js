33b454dcad7f7064997794c208ef3ccbf452fac0
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2021-11-16 20:04:40 +0300

    fetch added

--- js-scripts/array.js
+++ js-scripts/array.js
@@ -1,12 +1,20 @@
 let startIndex = parseInt(localStorage.getItem('startIndex')) || 0;
+let url = 'https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json';
 
-let item =[]
-$.getJSON({
-    url: 'https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json'
-  }).then(function (i) {
-    item=i;
-    getPersonForView(item)
-    createSelect()
-    disableBtn(startIndex)
-  })
+let item;
+
+const currentloginid = async () => {
+  const response = await fetch(url)
+
+  item = await response.json()
+  console.log(item)
+  await getPersonForView(item)
+  await createSelect()
+  await disableBtn(startIndex)
+    return item
+}
+
+currentloginid()
+  
+ 
   
